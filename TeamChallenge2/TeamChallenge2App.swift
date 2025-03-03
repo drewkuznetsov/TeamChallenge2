f66621f4f731e9266fcd501347ad38ce5b0a32c0
DrewKuznetsov <8150309@gmail.com>
2025-03-03 20:49:57 +0300

    Initial Commit

---
+++ TeamChallenge2/TeamChallenge2App.swift
@@ -0,0 +1,17 @@
+//
+//  TeamChallenge2App.swift
+//  TeamChallenge2
+//
+//  Created by Андрей Кузнецов on 03.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct TeamChallenge2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
